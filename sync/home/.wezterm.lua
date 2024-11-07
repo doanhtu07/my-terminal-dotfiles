3a0a32f20f19dfda735fd53f7cd8bbf7e6f67ac0
doanhtu07 <doanhtu07@gmail.com>
2024-11-07 10:06:44 -0600

    Increase fps for wezterm

--- sync/home/.wezterm.lua
+++ sync/home/.wezterm.lua
@@ -6,7 +6,7 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.max_fps = 120
 
 -- For example, changing the color scheme:
 config.colors = {
