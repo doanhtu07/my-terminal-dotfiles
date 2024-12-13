851688cd24b0aa908b4b31d614a9263434df8470
doanhtu07 <doanhtu07@gmail.com>
2024-12-13 14:19:51 -0600

    Update bufferline

--- sync/nvim/lua/toodope/core/keymaps.lua
+++ sync/nvim/lua/toodope/core/keymaps.lua
@@ -30,6 +30,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tc", "<cmd>BufferLinePick<CR>", { desc = "Choose tab" }) --  choose tab
 
 -- indent and re-select
 keymap.set("x", "<", "<gv", { desc = "Indent left" })
