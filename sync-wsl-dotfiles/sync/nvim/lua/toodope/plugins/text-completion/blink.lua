41f61303335237bb258880dfcaa596dc4f9e8ce2
doanhtu07 <doanhtu07@gmail.com>
2025-02-19 10:44:20 -0600

    Update plugins

--- sync-wsl-dotfiles/sync/nvim/lua/toodope/plugins/text-completion/blink.lua
+++ sync-wsl-dotfiles/sync/nvim/lua/toodope/plugins/text-completion/blink.lua
@@ -64,10 +64,10 @@ return {
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			default = { "lsp", "path", "snippets", "buffer" },
-
-			-- Disable cmdline completions
-			cmdline = {},
 		},
+
+		-- Disable cmdline completions
+		cmdline = { enabled = false },
 	},
 
 	opts_extend = { "sources.default" },
